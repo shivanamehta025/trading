e14855d7fb13584105327ac471bdd1fe9cb99fc5
shivanamehta025 <gi.developer05@gmail.com>
2026-07-17 17:19:15 +0530

    added top customer growth api

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,6 +3,151 @@ const router = express.Router();
 
 const sql = require("mssql");
 const { getPool } = require("../config/db");
+router.post("/sales-dashboard", async (req, res) => {
+
+  try {
+
+    const {
+      databaseName,
+      userId
+    } = req.body;
+
+    const pool =
+      await getPool(databaseName);
+
+    const result =
+      await pool.request()
+
+      .input(
+        "WHAT",
+        sql.VarChar,
+        "SALES"
+      )
+
+      .input(
+        "USERID",
+        sql.VarChar,
+        userId
+      )
+
+      .execute(
+        "A_SP_FOR_DASHBOARD_APP"
+      );
+
+    res.json({
+
+  success: true,
+
+  MTDSALES:
+      result.recordsets[0][0]
+          ?.MTDSALES ?? 0,
+           
+  TODAYSALES:
+      result.recordsets[1][0]
+          ?.TODAYSALES ?? 0,
+
+  TOTALCHALLANS:
+      result.recordsets[2][0]
+          ?.TOTALCHALLANS ?? 0,
+
+  CUSTOMERS:
+      result.recordsets[3][0]
+          ?.CUSTOMERS ?? 0,
+
+ 
+  // ========================================
+// MONTH
+// ========================================
+
+CURRENTMTD:
+    result.recordsets[5]?.[0]?.CurrentMTD ?? 0,
+
+LASTMTD:
+    result.recordsets[5]?.[0]?.LastMTD ?? 0,
+
+MTDGROWTHPERCENT:
+    result.recordsets[5]?.[0]?.MTDGrowthPercent ?? 0,
+
+
+// ========================================
+// QUARTER
+// ========================================
+
+CURRENTQTD:
+    result.recordsets[5]?.[0]?.CurrentQTD ?? 0,
+
+LASTQTD:
+    result.recordsets[5]?.[0]?.LastQTD ?? 0,
+
+QTDGROWTHPERCENT:
+    result.recordsets[5]?.[0]?.QTDGrowthPercent ?? 0,
+
+QUARTERNO:
+    result.recordsets[5]?.[0]?.QuarterNo ?? 0,
+
+CURRENTQUARTERSTART:
+    result.recordsets[5]?.[0]?.CurrentQuarterStart ?? null,
+
+CURRENTQUARTEREND:
+    result.recordsets[5]?.[0]?.CurrentQuarterEnd ?? null,
+
+PREVIOUSQUARTERSTART:
+    result.recordsets[5]?.[0]?.PreviousQuarterStart ?? null,
+
+PREVIOUSQUARTEREND:
+    result.recordsets[5]?.[0]?.PreviousQuarterEnd ?? null,
+
+
+// ========================================
+// FINANCIAL YEAR
+// ========================================
+
+CURRENTYTD:
+    result.recordsets[5]?.[0]?.CurrentYTD ?? 0,
+
+LASTYTD:
+    result.recordsets[5]?.[0]?.LastYTD ?? 0,
+
+YTDGROWTHPERCENT:
+    result.recordsets[5]?.[0]?.YTDGrowthPercent ?? 0,
+
+  MONTHLYTREND:
+      result.recordsets[4] ?? [],
+
+      DUEAMOUNT:
+      result.recordsets[6]?.[0]?.DUEAMOUNT ?? 0,
+
+      TOPDUECUSTOMERS: result.recordsets[7] ?? [],
+
+      TODAYSALESLIST: result.recordsets[8] ?? [],
+      
+      //weeklyTrend : result.recordsets[9]?? [],
+
+      TOPDUECUSTOMERSBYDUEDAYS: result.recordsets[9] ?? [],
+
+      CurrentQty: result.recordsets[11][0]?.CurrentQty ?? 0,
+      avgLast3MonthsQty:result.recordsets[11][0]?.avgLast3MonthsQty ?? 0,
+      TargetQty: result.recordsets[11][0]?.TargetQty ?? 0,
+      RemainingQty: result.recordsets[11][0]?.RemainingQty ?? 0,
+      ExcessQty: result.recordsets[11][0]?.ExcessQty ?? 0,
+      AchievementPercent: result.recordsets[11][0]?.AchievementPercent ?? 0,
+      //MonthlyTarget: result.recordsets[11][0]?.MonthlyTarget ?? 0,
+     // MTDTarget: result.recordsets[11][0]?.MTDTarget ?? 0,
+
+});
+
+  } catch (err) {
+
+    console.log(err);
+
+    res.status(500).json({
+
+      success: false,
+
+      message: err.message,
+    });
+  }
+});
 router.post("/top-growing-products", async (req, res) => {
 
   const { databaseName, userId } = req.body;
@@ -58,6 +203,31 @@ router.post('/customer-health-details', async (req, res) => {
       success: false,
       message: err.message,
     });
+
+  }
+
+});
+
+router.post("/top-growing-customers", async (req, res) => {
+
+  const { databaseName, userId } = req.body;
+
+  try {
+
+    const pool = await getPool(databaseName);
+
+    const result = await pool.request()
+      .input("WHAT", sql.VarChar, "TOP_GROWING_CUSTOMERS")
+      .input("USERID", sql.VarChar, userId)
+      .execute("A_SP_FOR_DASHBOARD_APP");
+
+    res.json(result.recordset);
+
+  } catch (err) {
+
+    console.error(err);
+
+    res.status(500).json({ error: err.message });
 
   }
 
